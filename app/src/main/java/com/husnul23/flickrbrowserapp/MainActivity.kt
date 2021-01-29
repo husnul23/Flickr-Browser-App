1405be270a9ea78432ea3e6952fd86c1b5075c9c
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-01-29 21:58:39 +0700

    add url to get data

--- app/src/main/java/com/husnul23/flickrbrowserapp/MainActivity.kt
+++ app/src/main/java/com/husnul23/flickrbrowserapp/MainActivity.kt
@@ -16,10 +16,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        val getRawData = GetRawData()
+        getRawData.execute("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,oreo&format=json&nojsoncallback=1")
+
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
         Log.d(TAG, "onCreate ends")
     }
 
@@ -43,5 +46,13 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "MainActivity"
+    }
+
+    fun onDownloadComplete(data: String, status: DownloadStatus) {
+        if (status == DownloadStatus.OK) {
+            Log.d(TAG, "onDownloadComplete called, data is $data")
+        } else {
+            Log.d(TAG, "onDownloadComplete failed with status $status, Error mesage is: $data")
+        }
     }
 }
