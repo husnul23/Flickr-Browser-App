c9fb22dfa33c6442b51fbcd4e7c352e820f6334b
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-01-31 14:40:37 +0700

    add Photo class

---
+++ app/src/main/java/com/husnul23/flickrbrowserapp/Photo.kt
@@ -0,0 +1,9 @@
+package com.husnul23.flickrbrowserapp
+
+class Photo(val title: String, val author: String, val authorId: String,
+            val link: String, val tags: String, val image: String) {
+    
+    override fun toString(): String {
+        return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tags='$tags', image='$image')"
+    }
+}
