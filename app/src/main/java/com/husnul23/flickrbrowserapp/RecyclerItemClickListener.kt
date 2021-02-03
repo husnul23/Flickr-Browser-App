c2639a7fb9a19a4c296f2665af95acafd7ea9046
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-03 19:29:41 +0700

    add gesture methodto handle  when single and long press

--- app/src/main/java/com/husnul23/flickrbrowserapp/RecyclerItemClickListener.kt
+++ app/src/main/java/com/husnul23/flickrbrowserapp/RecyclerItemClickListener.kt
@@ -2,8 +2,10 @@ package com.husnul23.flickrbrowserapp
 
 import android.content.Context
 import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerItemClickListener(context: Context,
@@ -16,9 +18,31 @@ class RecyclerItemClickListener(context: Context,
         fun onItemLongClick(view: View, position: Int)
     }
 
+    // add the gestureDetector
+
+    private val gestureDetector = GestureDetectorCompat(context, object : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.d(TAG, "onSingleTapUp: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, "onSingleTapUp: calling listener.onItemClick")
+            listener.onItemClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.d(TAG, "onLongPress: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, "onLongPress: calling listener.onItemLongClick")
+            listener.onItemLongClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+        }
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         Log.d(TAG, "onInterceptTouchEvent: Start $e")
+        val result = gestureDetector.onTouchEvent(e)
+        Log.d(TAG, "onInterceptTouchEvent() returning: $result")
 //        return super.onInterceptTouchEvent(rv, e)
-        return true
+        return result
     }
 }
