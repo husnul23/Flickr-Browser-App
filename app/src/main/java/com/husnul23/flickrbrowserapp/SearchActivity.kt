2e9e42ceea840184633d5d6056878b4f64040a72
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-03 23:01:57 +0700

    update extend class to BaseActivity

--- app/src/main/java/com/husnul23/flickrbrowserapp/SearchActivity.kt
+++ app/src/main/java/com/husnul23/flickrbrowserapp/SearchActivity.kt
@@ -1,17 +1,16 @@
 package com.husnul23.flickrbrowserapp
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : BaseActivity() {
+    private val TAG = "SearchActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"onCreate: ends")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        activateToolbar(true)
+        Log.d(TAG,"onCreate: ends")
     }
 }
