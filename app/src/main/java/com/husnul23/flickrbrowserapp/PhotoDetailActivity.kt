9c393d7c9e0cda7a291b3968ceca204eeaf577aa
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-05 23:36:33 +0700

    add intent to photo detail to get data

--- app/src/main/java/com/husnul23/flickrbrowserapp/PhotoDetailActivity.kt
+++ app/src/main/java/com/husnul23/flickrbrowserapp/PhotoDetailActivity.kt
@@ -1,6 +1,9 @@
 package com.husnul23.flickrbrowserapp
 
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.brows.*
+import kotlinx.android.synthetic.main.content_photo_detail.*
 
 class PhotoDetailActivity : BaseActivity() {
 
@@ -8,5 +11,16 @@ class PhotoDetailActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_detail)
         activateToolbar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+
+        photo_title.text = photo.title
+        photo_tag.text = photo.tags
+        photo_author.text = photo.author
+
+        Picasso.with(this).load(photo.link)
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(imageView)
     }
 }
