435eaed9f95a14ecae39bce6b0eba5bd81363b3a
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-05 23:49:26 +0700

    change parcelable instead of serializable

--- app/src/main/java/com/husnul23/flickrbrowserapp/PhotoDetailActivity.kt
+++ app/src/main/java/com/husnul23/flickrbrowserapp/PhotoDetailActivity.kt
@@ -2,7 +2,6 @@ package com.husnul23.flickrbrowserapp
 
 import android.os.Bundle
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.brows.*
 import kotlinx.android.synthetic.main.content_photo_detail.*
 
 class PhotoDetailActivity : BaseActivity() {
@@ -12,7 +11,7 @@ class PhotoDetailActivity : BaseActivity() {
         setContentView(R.layout.activity_photo_detail)
         activateToolbar(true)
 
-        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+        val photo = intent.getParcelableExtra<Photo>(PHOTO_TRANSFER) as Photo
 
         photo_title.text = photo.title
         photo_tag.text = photo.tags
