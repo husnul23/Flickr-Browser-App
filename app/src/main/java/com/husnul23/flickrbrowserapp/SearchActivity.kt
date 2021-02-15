d800e29aed97c826863f7a3c0a96d8845663dce5
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-15 15:07:21 +0700

    add searchView library to Search activity

--- app/src/main/java/com/husnul23/flickrbrowserapp/SearchActivity.kt
+++ app/src/main/java/com/husnul23/flickrbrowserapp/SearchActivity.kt
@@ -2,9 +2,12 @@ package com.husnul23.flickrbrowserapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 
 class SearchActivity : BaseActivity() {
     private val TAG = "SearchActivity"
+    private var searchView: SearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG,"onCreate: ends")
@@ -12,5 +15,10 @@ class SearchActivity : BaseActivity() {
         setContentView(R.layout.activity_search)
         activateToolbar(true)
         Log.d(TAG,"onCreate: ends")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        return true
     }
 }
